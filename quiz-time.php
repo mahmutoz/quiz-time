52d47deb2319c19e611f16fc2a02e8da7436f825
Mahmut ÖZ <mahmutoz8197@gmail.com>
2025-02-16 19:00:12 +0300

    Add dark mode styles for quiz plugin components

--- quiz-time.php
+++ quiz-time.php
@@ -3,8 +3,14 @@
  * Plugin Name: Quiz Time
  * Description: Çoktan seçmeli test oluşturma ve yönetme eklentisi
  * Version: 1.0.0
- * Author: Your Name
+ * Author: Mahmut Öz
+ * Author URI: https://github.com/mahmutoz
+ * License: GPL v2 or later
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain: quiz-time
+ * Domain Path: /languages
+ * Requires at least: 5.8
+ * Requires PHP: 7.4
  */
 
 if (!defined('ABSPATH')) {
